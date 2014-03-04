ff3551d7313ed01d0df17d7ddad01d9103205d79
BassT <sebastian.richter@student.kit.edu>
2014-03-04 16:31:08 -0500

    elaborated on example

--- main.py
+++ main.py
@@ -24,19 +24,21 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 
 sparql = SPARQLWrapper("http://dbpedia.org/sparql")
 dbpedia = quepy.install("dbpedia")
+bottle.debug(True)
 
 # quepy.set_loglevel("DEBUG")
 
 @bottle.route("/")
 def index():
+    yield u"<p>Here is a <a href=\"http://bottlepy.org/\">bottle</a> app under construction. This example converts the question 'What is a blowtorch?' to a SPARQL query using <a href=\"http://quepy.machinalis.com/\">quepy</a>, queries dbpedia and returns the results (the RDF literals retrieved by rdfs:comment).</p>"
     target, query, metadata = dbpedia.get_query("what is a blowtorch?")
-    yield query + "\n"
+    yield u"<p>" + query + u"</p>"
     sparql.setQuery(query)
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()
 
     for result in results["results"]["bindings"]:
-        yield(result["x1"]["value"])
+        yield "<p>" + result["x1"]["value"] + "</p>"
     return
 
 bottle.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
