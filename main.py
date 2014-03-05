457907779b5a9d3221be4c299966fb7ee1079df8
Fatima <faa@yorku.ca>
2014-03-05 12:39:23 -0500

    Changing the main to have a typed question

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ import time
 import random
 import datetime
 import bottle
+#from bottle import route, run, template
 import os
 
 import quepy
@@ -41,4 +42,17 @@ def index():
         yield "<p>" + result["x1"]["value"] + "</p>"
     return
 
+@bottle.route("/question/<q>")
+def question(q):
+    yield u"<p>This example converts the question " + q + u"to a SPARQL query using <a href=\"http://quepy.machinalis.com/\">quepy</a>, queries dbpedia and returns the results (the RDF literals retrieved by rdfs:comment).</p>"
+    target, query, metadata = dbpedia.get_query(q)
+    yield u"<p>" + query + u"</p>"
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+
+    for result in results["results"]["bindings"]:
+        yield "<p>" + result["x1"]["value"] + "</p>"
+    return
 bottle.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
