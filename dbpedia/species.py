e38aefe00cca74d53ffd46f755f008c71291bff9
Fatima <faa@yorku.ca>
2014-04-04 23:10:38 -0400

    Take out WhatIs quesiton from Species, because it affect WhatIs question in Basic.py and consider every WhatIs question to be species

--- dbpedia/species.py
+++ dbpedia/species.py
@@ -13,34 +13,8 @@ Species related regex.
 from refo import Question, Group
 from quepy.dsl import HasKeyword
 from quepy.parsing import Lemma, Lemmas, Pos, QuestionTemplate
-from dbpedia.dsl import IsSpecies, DefinitionOf, FiberOf, CarbsOf, FatOf, ProteinOf, SugarOf, IngredientOf, LabelOf
+from dbpedia.dsl import IsSpecies, FiberOf, CarbsOf, FatOf, ProteinOf, SugarOf, IngredientOf, LabelOf
 
-
-"""
-class Species(Particle):
-    regex = Plus(Pos("DT") | Pos("NN") | Pos("NNS") | Pos("NNP") | Pos("VBD") | Pos("NNPS"))
-
-    def interpret(self, match):
-        name = match.words.tokens
-       # print name
-        return HasKeyword(name)
-        #return name
- """  
-
-class WhatIs(QuestionTemplate):
-    """
-    Ex: "What is an apple?"
-    """
-    
-    regex = Lemma("what") + Lemma("be") + Question(Pos("DT")) + Group(Pos("NNP"), 'species') + Question(Pos("."))
-            
-    def interpret(self, match):
-        label = match.species.tokens
-        speciesName = IsSpecies()+ HasKeyword(label)
-        print label
-        name = DefinitionOf(speciesName)
-        return name, "enum"
-    
 
 class FiberOfQuestion(QuestionTemplate):
     """
