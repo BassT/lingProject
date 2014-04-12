e04067e3886d46736a2ec5ea740d1e16b703b76b
Fatima <faa@yorku.ca>
2014-04-12 13:48:07 -0400

    Take out WhereIsQuestion and add it to NewBasic, beacuase it had my update about hotels

--- dbpedia/NewBasic.py
+++ dbpedia/NewBasic.py
@@ -14,11 +14,11 @@ New Basic questions that are not supproted in basic.py
 from refo import Question, Plus
 from quepy.dsl import HasKeyword
 from quepy.parsing import Lemma, Lemmas, Pos, QuestionTemplate, Particle
-from dbpedia.dsl import ExternalLinkOf, DefinitionOf
+from dbpedia.dsl import ExternalLinkOf, DefinitionOf, LocationOf, NameOf
 
 class Thing(Particle):
-    regex = Plus(Pos("CD")| Pos("NN") | Pos("JJ") |Pos("NNS") | Pos("DT") | Pos("NNP") | Pos("NNP") | Pos("NNP") | Pos("IN"))
-    
+    regex = Plus(Pos("CD") | Pos("JJ") |Pos("NNS") | Pos("DT") | Pos("NNP") | Pos("IN"))
+
     def interpret(self, match):
         name = match.words.tokens
         return HasKeyword(name)
@@ -27,10 +27,10 @@ class Thing(Particle):
 class WhatIs(QuestionTemplate):
     """
     Regex for questions about what is a thing
-    Ex: "What is a car"
-        "What is York University?"
+    Ex: "What is York University?"
         "What is University of Toronot?"
         "What is Jumeirah Beach Hotel?"
+        "What is Crowne Plaza?"
     """
 
     regex = Lemma("what") + Lemma("be") + Question(Pos("DT")) + Thing() + Question(Pos("."))
@@ -44,7 +44,7 @@ class LinkQuestion(QuestionTemplate):
     """
     Regex for questions about the website for a thing
     Ex: "what is the link of Jumeirah Beach Hotel?"
-        "what is the website of Jumeirah Beach Hotel?"
+        "What is the website of InterContinental Hong Kong?"
         "what is the link of York University?"
         "what is the website of University of Toronto?"
     """
@@ -55,4 +55,26 @@ class LinkQuestion(QuestionTemplate):
     def interpret(self, match):
         Link = ExternalLinkOf(match.thing)
         return Link, "enum"
+ 
     
+class LocationOfQuestion(QuestionTemplate):
+    """
+    Ex: "where in the world is the Eiffel Tower"
+        "Where is Jumeirah Beach Hotel?"
+        "Where is the location of Marriott London Park Lane?"
+        "Show me the location of Jumeirah Beach Hotel?"
+        "Give me the location of Jumeirah Beach Hotel?"
+    """
+
+                  
+    regex = Lemma("where") + Question(Lemmas("in the world")) + Lemma("be") + Question(Pos("DT")) + Thing() + Question(Pos(".")) | \
+        (Lemma("where") + Lemma("be") + Thing() + Question(Pos("."))) | \
+        (Question(Lemmas("where be")) + Lemma("the") + Lemma("location") + Pos("IN") + Thing() + Question(Pos("."))) | \
+        (Question(Lemma("show")|Lemma("give")) + Lemma("me") + Lemma("the") + Lemma("location") + Pos("IN") + Thing() + Question(Pos("."))) 
+            
+
+    def interpret(self, match):
+        location = LocationOf(match.thing)
+        location_name = NameOf(location)
+
+        return location_name, "enum"
